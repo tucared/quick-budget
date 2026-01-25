096f2db31e5bf6e279e68512a0e66a8f1babb43d
Tucared <max.perdrigeat@gmail.com>
2026-01-25 17:27:23 +0100

    feat: all local dev origins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  allowedDevOrigins: ["192.168.1.*"],
+};
 
-module.exports = nextConfig
+export default nextConfig;
